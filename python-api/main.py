f92cd8260d4efbd800c3b3ab7146da54cbc446b8
nachotopa <nachotopa462005@gmail.com>
2026-05-18 14:37:54 +0200

    main py nuevo

--- python-api/main.py
+++ python-api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 import redis
 import os
+import json
 
 app = FastAPI()
-
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
@@ -15,7 +15,6 @@ r = redis.Redis(
     password=REDIS_PASSWORD,
     decode_responses=True
 )
-
 
 inventario = [
     {"id": 1, "producto": "Teclado", "stock": 10},
@@ -33,7 +32,47 @@ def get_inventario():
     return inventario
 
 
-@app.get("/redis")
-def redis_test():
-    r.set("mensaje", "Redis funcionando")
-    return {"redis": r.get("mensaje")}
+@app.get("/cache")
+def cache_logs():
+
+    cache = r.get("logs_cache")
+
+    if cache:
+        return {
+            "fuente": "redis",
+            "data": json.loads(cache)
+        }
+
+    logs = {
+        "errores": 15,
+        "warnings": 8,
+        "ips": ["192.168.1.10", "10.0.0.5"]
+    }
+
+    r.set("logs_cache", json.dumps(logs))
+
+    return {
+        "fuente": "api",
+        "data": logs
+    }
+
+
+@app.post("/ips/{ip}")
+def add_ip(ip: str):
+
+    r.sadd("ips_sospechosas", ip)
+
+    return {
+        "mensaje": "IP agregada",
+        "ip": ip
+    }
+
+
+@app.get("/ips")
+def get_ips():
+
+    ips = list(r.smembers("ips_sospechosas"))
+
+    return {
+        "ips_sospechosas": ips
+    }
